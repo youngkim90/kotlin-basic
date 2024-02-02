d960c7cd551942290b652afa2aea580633774e0b
young-kim <ceo.youngkim@gmail.com>
2024-02-02 12:41:36 +0900

    데이터 클래스 주석 수정

--- src.main.kotlin/advanced/2_DataClass.kt
+++ src.main.kotlin/advanced/2_DataClass.kt
@@ -7,13 +7,15 @@ data class Person(val name: String, val age: Int)
 fun main() {
     val person1 = Person(name = "tony", age = 20)
     val person2 = Person(name = "tony", age = 20)
+    // data class는 비교 시 equals로 비교하기 때문에 내용이 동일하면 true
+    println(person1 == person2)
 
-    println(person1 == person2) // data class는 비교 시 equals로 비교하기 때문에 내용이 동일하면 true
-
+    // data class는 hashcode를 자동으로 생성하기 때문에 동일한 객체로 인식
     val set = hashSetOf(person1)
-    println(set.contains(person2)) // data class는 hashcode를 자동으로 생성하기 때문에 동일한 객체로 인식
+    println(set.contains(person2))
 
-    println(person1.toString()) // toString()이 포함되어 있음(생략가능)
+    // toString()이 포함되어 있음(생략가능)
+    println(person1.toString())
 
     // 가변 객체를 사용하여 값을 변경하면 프로퍼티 내부 값이 변경되고 추적이 어려워짐. 멀티스레드 환경에서 문제가 발생할 수 있음.
     // 불변 객체 상태로 복사하여 사용하면 안전함
@@ -22,6 +24,7 @@ fun main() {
 
     println("이름=${person1.component1()}, 나이=${person1.component2()}")
 
-    val (name, age) = person1 // 구조 분해 선언 방식
+    // 구조 분해 선언 방식
+    val (name, age) = person1
     println("이름=$name, 나이=$age")
 }
